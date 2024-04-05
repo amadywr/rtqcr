ddcaec305d6147f9d8b27c80b81bb9aa65f46f40
Ahmad Yawari <k_han21@yahoo.com>
2024-04-05 22:32:49 +1100

    a

--- src/components/RecontProjects.jsx
+++ src/components/RecontProjects.jsx
@@ -1,12 +1,68 @@
 import React from 'react';
+import styles from './RecentProjects.module.css';
+import photo from '../assets/epoxy_flooring.jpg';
 
 function RecontProjects() {
   return (
-    <div>
+    <section className={`${styles.recent_projects_section}`}>
       <h1>Recent Projects</h1>
 
-      <div></div>
-    </div>
+      <div className={styles.projects_div}>
+        <div className={styles.project_div}>
+          <div>
+            <p>Penrith</p>
+            <h2>Concrete Resurfacing</h2>
+          </div>
+          <div className={styles.project1_before_div}>
+            <img
+              src={photo}
+              alt="concrete resurfacing"
+              className={styles.project1_before}
+            />
+          </div>
+
+          <img
+            src={photo}
+            alt="concrete resurfacing"
+            className={styles.project1_after}
+          />
+        </div>
+
+        <div className={styles.project_div}>
+          <div>
+            <p>Penrith</p>
+            <h2>Concrete Resurfacing</h2>
+          </div>
+          <img
+            src={photo}
+            alt="concrete resurfacing"
+            className={styles.project1_before}
+          />
+          <img
+            src={photo}
+            alt="concrete resurfacing"
+            className={styles.project1_after}
+          />
+        </div>
+
+        <div className={styles.project_div}>
+          <div>
+            <p>Penrith</p>
+            <h2>Concrete Resurfacing</h2>
+          </div>
+          <img
+            src={photo}
+            alt="concrete resurfacing"
+            className={styles.project1_before}
+          />
+          <img
+            src={photo}
+            alt="concrete resurfacing"
+            className={styles.project1_after}
+          />
+        </div>
+      </div>
+    </section>
   );
 }
 
