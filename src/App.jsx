bdf77e23d9766a13808fc8bfd146cd4eee32fbb8
Ahmad Yawari <k_han21@yahoo.com>
2024-04-25 19:59:32 +1000

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homepage from './pages/Homepage/Homepage';
-import Navbar from './components/general_components/Navbar/Navbar';
-import EpoxyFlooring from './pages/EpoxyFlooring/EpoxyFlooring';
-import SprayCreteFloor from './pages/SprayCreteFloor/SprayCreteFloor';
-import ConcreteSealing from './pages/ConcreteSealing/ConcreteSealing';
-import ConcretePolishing from './pages/ConcretePolishing/ConcretePolishing';
-import NotFound from './pages/NotFound/NotFound';
-import Footer from './components/general_components/Footer/Footer';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Homepage from './pages/Homepage/Homepage';
+// import Navbar from './components/general_components/Navbar/Navbar';
+// import EpoxyFlooring from './pages/EpoxyFlooring/EpoxyFlooring';
+// import SprayCreteFloor from './pages/SprayCreteFloor/SprayCreteFloor';
+// import ConcreteSealing from './pages/ConcreteSealing/ConcreteSealing';
+// import ConcretePolishing from './pages/ConcretePolishing/ConcretePolishing';
+// import NotFound from './pages/NotFound/NotFound';
+// import Footer from './components/general_components/Footer/Footer';
 import ComingSoon from './pages/ComingSoon/ComingSoon';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Navbar />
 
         <Routes>
@@ -21,12 +21,13 @@ function App() {
           <Route path="/epoxy_flooring" element={<EpoxyFlooring />} />
           <Route path="/concrete_sealing" element={<ConcreteSealing />} />
           <Route path="/concrete_polishing" element={<ConcretePolishing />} />
-          <Route path="/coming_soon" element={<ComingSoon />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
 
         <Footer />
-      </BrowserRouter>
+      </BrowserRouter> */}
+
+      <ComingSoon />
     </div>
   );
 }
